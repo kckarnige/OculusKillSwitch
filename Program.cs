5d676f595b54bb70ee7c5caadc1575e77f9e33c2
KayoticCarnige <32397453+kckarnige@users.noreply.github.com>
2023-10-18 10:30:33 -0400

    Version 1.5.0
    Added prompt to auto-download Oculus Killer if it isn't detect :)

--- Program.cs
+++ Program.cs
@@ -2,7 +2,8 @@
 
 using System.IO;
 using System.Security.Cryptography;
-using System.Windows;
+using System.Diagnostics;
+using System.Net;
 
 static class Program
 {
@@ -23,25 +24,77 @@ static class Program
     static void Main()
     {
         Application.SetHighDpiMode(HighDpiMode.PerMonitorV2);
-        string fileHash = GetMD5Hash("OculusDash.exe");
+        string activeFileHash;
+        string backupFileHash;
+        bool whoops = false;
+        try
+        {
+            activeFileHash = GetMD5Hash("OculusDash.exe");
+        }
+        catch
+        {
+            whoops = true;
+            activeFileHash = "null";
+            Process.Start("explorer.exe", @"C:\Program Files\Oculus\Support\oculus-dash\dash\bin");
+            MessageBox.Show("Not in the right directory bucko, it goes here.\nAfter I close, would you be a doll and move me there?", "Oculus Kill Switch", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+        try
+        {
+            backupFileHash = GetMD5Hash("OculusDash.exe.bak");
+        }
+        catch (Exception)
+        {
+            whoops = true;
+            if (activeFileHash == "9DB7CC8B646A01C60859B318F85E65D0")
+            {
+                MessageBox.Show("Oculus Dash backup couldn't be found. If it's in a different directory, move it here. If you don't have it, you may need to reinstall the Oculus app to get it back.", "Oculus Kill Switch", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (activeFileHash == "DD65C6B1E5578AE812966798101F1379")
+            {
+                DialogResult Dialog1 = MessageBox.Show("The Oculus Killer isn't installed, or the backup file has been renamed. Would you like for me to download it for you?", "Oculus Kill Switch", MessageBoxButtons.YesNo, MessageBoxIcon.Error);
+                if (Dialog1 == DialogResult.Yes)
+                {
+                    using (WebClient client = new WebClient())
+                    {
+                        client.DownloadFile("https://github.com/LibreQuest/OculusKiller/releases/latest/download/OculusDash.exe", "OculusDash.exe.bak");
+                    }
+
+                }
+            }
+        }
         string killerEnabled;
 
-        if (fileHash == "9DB7CC8B646A01C60859B318F85E65D0") {
+        if (activeFileHash == "9DB7CC8B646A01C60859B318F85E65D0")
+        {
             killerEnabled = "enabled.";
-        } else
+        }
+        else
         {
             killerEnabled = "disabled.";
-        } ;
-        DialogResult Dialog0 = MessageBox.Show("Toggle Oculus Killer?\n" + "It's currently " + killerEnabled, "Oculus Kill Switch", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+        };
+
+        DialogResult Dialog0;
+
+        if (whoops != true)
+        {
+            Dialog0 = MessageBox.Show("Toggle Oculus Killer?\n" + "It's currently " + killerEnabled, "Oculus Kill Switch", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+        }
+        else
+        {
+            Dialog0 = DialogResult.None;
+        }
+
+        // It'll be fiiiiiine
         if (Dialog0 == DialogResult.OK)
         {
-            if (fileHash != "9DB7CC8B646A01C60859B318F85E65D0")
+            if (activeFileHash != "9DB7CC8B646A01C60859B318F85E65D0")
             {
                 File.Move("OculusDash.exe.bak", "tempkill.exe");
                 File.Move("OculusDash.exe", "OculusDash.exe.bak");
                 File.Move("tempkill.exe", "OculusDash.exe");
                 MessageBox.Show("Successfully enabled the Oculus Killer!", "Oculus Kill Switch", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            } else
+            }
+            else
             {
                 File.Move("OculusDash.exe.bak", "tempkill.exe");
                 File.Move("OculusDash.exe", "OculusDash.exe.bak");
@@ -49,6 +102,5 @@ static class Program
                 MessageBox.Show("Successfully disabled the Oculus Killer!", "Oculus Kill Switch", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
         }
-
-    }    
+    }
 }
--- README.md
+++ README.md
@@ -6,8 +6,17 @@
 
 <h3 align="center">Toggle Oculus Killer, making it possible to still play your Oculus games, incase you have any.</h3>
 
-> _This program was made with the assumption that you followed [the Oculus Killer installation guide](https://github.com/LibreQuest/OculusKiller#installation), and backed up your Oculus Dash executable as `OculusDash.exe.bak` as per the instructions._
+> _This program was made with the assumption that you followed the [Oculus Killer install guide](https://github.com/LibreQuest/OculusKiller#installation), and backed up your Oculus Dash executable as `OculusDash.exe.bak` as per the instructions._
 
 > _Keep in mind, this is my first time coding in C#, that being said, if there are any bugs, [submit an issue](https://github.com/kckarnige/OculusKillSwitch/issues), and feel free to make [pull requests](https://github.com/kckarnige/OculusKillSwitch/pulls)._
 
-<h3 align="center"><a href="https://github.com/kckarnige/OculusKillSwitch/releases">[ [ Download ] ]</a></h3>
+
+## Installation
+
+1. Make sure you follow the [Oculus Killer install guide](https://github.com/LibreQuest/OculusKiller#installation) <u>***exactly***</u>, filenames and all.
+
+2. [Download](https://github.com/kckarnige/OculusKillSwitch/releases) Oculus Kill Switch
+
+3. Move it to the same directory where you installed Oculus Killer, should at `C:\Program Files\Oculus\Support\oculus-dash\dash\bin` by default.
+
+4. That's it, just make sure to make a shortcut to it so you can access it at any time!
