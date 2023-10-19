da83f5f334a89461758914c31ed2905654f24edc
KayoticCarnige <32397453+kckarnige@users.noreply.github.com>
2023-10-19 09:47:20 -0400

    Now only runs if Oculus Dash isn't running
    Still need to do a bit more testing before release

--- Program.cs
+++ Program.cs
@@ -24,83 +24,91 @@ static class Program
     static void Main()
     {
         Application.SetHighDpiMode(HighDpiMode.PerMonitorV2);
-        string activeFileHash;
-        string backupFileHash;
-        bool whoops = false;
-        try
+        Process[] DashCheck = Process.GetProcessesByName("OculusDash");
+        if (DashCheck.Length > 0 != true)
         {
-            activeFileHash = GetMD5Hash("OculusDash.exe");
-        }
-        catch
-        {
-            whoops = true;
-            activeFileHash = "null";
-            Process.Start("explorer.exe", @"C:\Program Files\Oculus\Support\oculus-dash\dash\bin");
-            MessageBox.Show("Not in the right directory bucko, it goes here.\nAfter I close, would you be a doll and move me there?", "Oculus Kill Switch", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-        }
-        try
-        {
-            backupFileHash = GetMD5Hash("OculusDash.exe.bak");
-        }
-        catch (Exception)
-        {
-            whoops = true;
-            if (activeFileHash == "9DB7CC8B646A01C60859B318F85E65D0")
+            string activeFileHash;
+            string backupFileHash;
+            bool whoops = false;
+            try
             {
-                MessageBox.Show("Oculus Dash backup couldn't be found. If it's in a different directory, move it here. If you don't have it, you may need to reinstall the Oculus app to get it back.", "Oculus Kill Switch", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                activeFileHash = GetMD5Hash("OculusDash.exe");
             }
-            else if (activeFileHash == "DD65C6B1E5578AE812966798101F1379")
+            catch
             {
-                DialogResult Dialog1 = MessageBox.Show("The Oculus Killer isn't installed, or the backup file has been renamed. Would you like for me to download it for you?", "Oculus Kill Switch", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
-                if (Dialog1 == DialogResult.Yes)
+                whoops = true;
+                activeFileHash = "null";
+                Process.Start("explorer.exe", @"C:\Program Files\Oculus\Support\oculus-dash\dash\bin");
+                MessageBox.Show("I'm not in the right directory, I go here.\nAfter I close, would you please move me?", "Oculus Kill Switch", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            try
+            {
+                backupFileHash = GetMD5Hash("OculusDash.exe.bak");
+            }
+            catch (Exception)
+            {
+                whoops = true;
+                if (activeFileHash == "9DB7CC8B646A01C60859B318F85E65D0")
                 {
-                    using (WebClient client = new WebClient())
+                    MessageBox.Show("Oculus Dash backup couldn't be found. If it's in a different directory, move it here. If you don't have it, you may need to reinstall the Oculus app to get it back.", "Oculus Kill Switch", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else if (activeFileHash == "DD65C6B1E5578AE812966798101F1379")
+                {
+                    DialogResult Dialog1 = MessageBox.Show("The Oculus Killer isn't installed, or the backup file has been renamed. Would you like for me to download it for you?", "Oculus Kill Switch", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                    if (Dialog1 == DialogResult.Yes)
                     {
-                        client.DownloadFile("https://github.com/LibreQuest/OculusKiller/releases/latest/download/OculusDash.exe", "OculusDash.exe.bak");
-                    }
+                        using (WebClient client = new WebClient())
+                        {
+                            client.DownloadFile("https://github.com/LibreQuest/OculusKiller/releases/latest/download/OculusDash.exe", "OculusDash.exe.bak");
+                        }
 
+                    }
                 }
             }
-        }
-        string killerEnabled;
+            string killerEnabled;
 
-        if (activeFileHash == "9DB7CC8B646A01C60859B318F85E65D0")
-        {
-            killerEnabled = "enabled.";
-        }
-        else
-        {
-            killerEnabled = "disabled.";
-        };
-
-        DialogResult Dialog0;
-
-        if (whoops != true)
-        {
-            Dialog0 = MessageBox.Show("Toggle Oculus Killer?\n" + "It's currently " + killerEnabled, "Oculus Kill Switch", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
-        }
-        else
-        {
-            Dialog0 = DialogResult.None;
-        }
-
-        // It'll be fiiiiiine
-        if (Dialog0 == DialogResult.OK)
-        {
-            if (activeFileHash != "9DB7CC8B646A01C60859B318F85E65D0")
+            if (activeFileHash == "9DB7CC8B646A01C60859B318F85E65D0")
             {
-                File.Move("OculusDash.exe.bak", "tempkill.exe");
-                File.Move("OculusDash.exe", "OculusDash.exe.bak");
-                File.Move("tempkill.exe", "OculusDash.exe");
-                MessageBox.Show("Successfully enabled the Oculus Killer!", "Oculus Kill Switch", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                killerEnabled = "enabled.";
             }
             else
             {
-                File.Move("OculusDash.exe.bak", "tempkill.exe");
-                File.Move("OculusDash.exe", "OculusDash.exe.bak");
-                File.Move("tempkill.exe", "OculusDash.exe");
-                MessageBox.Show("Successfully disabled the Oculus Killer!", "Oculus Kill Switch", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                killerEnabled = "disabled.";
+            };
+
+            DialogResult Dialog0;
+
+            if (whoops != true)
+            {
+                Dialog0 = MessageBox.Show("Toggle Oculus Killer?\n" + "It's currently " + killerEnabled, "Oculus Kill Switch", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
             }
+            else
+            {
+                Dialog0 = DialogResult.None;
+            }
+
+            // It'll be fiiiiiine
+            if (Dialog0 == DialogResult.OK)
+            {
+                if (activeFileHash != "9DB7CC8B646A01C60859B318F85E65D0")
+                {
+                    File.Move("OculusDash.exe.bak", "tempkill.exe");
+                    File.Move("OculusDash.exe", "OculusDash.exe.bak");
+                    File.Move("tempkill.exe", "OculusDash.exe");
+                    MessageBox.Show("Successfully enabled the Oculus Killer!", "Oculus Kill Switch", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    File.Move("OculusDash.exe.bak", "tempkill.exe");
+                    File.Move("OculusDash.exe", "OculusDash.exe.bak");
+                    File.Move("tempkill.exe", "OculusDash.exe");
+                    MessageBox.Show("Successfully disabled the Oculus Killer!", "Oculus Kill Switch", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+        else
+        {
+            MessageBox.Show("Please exit VR before switching your dash.\nIf this issue persists, try restarting the Oculus app.\nSettings > Beta > Restart Oculus", "Oculus Kill Switch", MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
     }
 }
